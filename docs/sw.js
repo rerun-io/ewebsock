b94aa0d68ad0a289dcec48170c98eb9dcd2821dd
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2022-03-07 12:34:18 +0100

    Update names in sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './viewer.js',
-  './viewer_bg.wasm',
+  './example_app.js',
+  './example_app_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
